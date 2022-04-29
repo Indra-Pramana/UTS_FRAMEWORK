4696ccfe4e068c29bce3d59fdc2128ce9c2c4833
Indra-Pramana <indrapermana01062001@gmail.com>
2022-04-29 10:07:29 -0700

    project uts

---
+++ app/Views/main/about.php
@@ -0,0 +1,69 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="info">
+				<center>
+					<h2><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Semnas Technology Behind Blockhain</h2>
+				</center>
+				<hr style="border: 1px grey solid;">
+			</div>
+			<div class="info-content">
+				<p style="text-align:justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					Fakultas Teknologi Informatika Universitas Pendidikan Nasional Denpasar menggelar Seminar Nasional Technology Behind Blockhain - 
+					Seminar ini terbuka untuk umum dan gratis (tidak dipungut biaya).
+					Informasi lengkap mengenai seminar ini sebagai berikut :
+				</p>
+				<table width="100%">
+					<tr>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-users"></i>&nbsp;&nbsp;Pembicara :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Anak Agung Rai Kencana, S.T., M.T.
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="far fa-clock"></i>&nbsp;&nbsp;Waktu :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Sabtu, 28 April 2022<br>
+									(Pukul 13.00 WITA - Selesai)
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class="kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;Tempat :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Ruang Seminar, Gedung Auditorium Undiknas
+								</p>
+								<br>
+							</div>
+						</td>
+						<td class=" kotak" width="25%" valign="top">
+							<center>
+								<p class="title"><i class="fas fa-phone-alt"></i>&nbsp;&nbsp;Contact Person :</p>
+							</center>
+							<div class="inner-content">
+								<p>
+									Indra (+62 838-3182-1293)
+								</p>
+								<br>
+							</div>
+						
+			</div>
+		</div>
+
+		<?= $this->endSection(); ?>
---
+++ app/Views/main/home.php
@@ -0,0 +1,19 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="konten">
+				<img class="pict1" src="<?= base_url('img/robot.png'); ?>">
+			</div>
+			<center>
+				<h3>Selamat Datang Di Semnas Technology Behind Blockhain</h3>
+				<p>Terbuka Untuk Umum - Penyelenggara : Undiknas Denpasar <b><span style="color:yellow;">*GRATIS</span></b>
+				<p>
+			</center>
+		</div>
+	</div>
+</div>
+
+<?= $this->endSection(); ?>
